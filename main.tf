39a0b64417d4e53d8c521586a6a435e0d749f00d
KarishmaAbruk <138876092+KarishmaAbruk@users.noreply.github.com>
2025-05-25 17:38:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_user" "jenkins_pipeline_user" {
+  name = var.iam_user_name
+  tags = {
+    Purpose = "JenkinsPipeline"
+    CreatedBy = "Terraform"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "jenkins_attach_policy" {
+  user       = aws_iam_user.jenkins_pipeline_user.name
+  policy_arn = "arn:as:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_access_key" "jenkins_user_key" {
+  user = aws_iam_user.jenkins_pipeline_user.name
+}
+
+output "access_key_id" {
+  value       = aws_iam_access_key.jenkins_user_key.id
+  sensitive   = true
+}
+
+output "secret_access_key" {
+  value       = aws_iam_access_key.jenkins_user_key.secret
+  sensitive   = true
+}
